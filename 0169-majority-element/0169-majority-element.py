9f5d8f2c6697e5b5233fcfb6b98aa75298559595
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2024-09-04 06:26:34 +0530

    Time: 174 ms (40.27%), Space: 17.9 MB (81.96%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        mapp = dict()
+        for i in nums:
+            if(i not in mapp):
+                mapp[i] = 1
+            else:
+                mapp[i] += 1
+        
+        res = list(sorted(mapp.items(), key=lambda x: x[1], reverse = True))
+        return res[0][0]
