eb8a1494724625c6c560ca82acc371d9b0f9bbe4
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2024-09-05 07:22:53 +0530

    Time: 714 ms (48.9%), Space: 20.6 MB (72.59%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,13 +5,23 @@ class Solution:
         for i in range(0, len(nums)):
             if(i != 0 and nums[i] == nums[i-1]):
                 continue
-            mapp = set()
-            for j in range(i+1, len(nums)):
-                req = -(nums[i] + nums[j])
-                temp = [nums[i], req, nums[j]]
-                if(req in mapp and temp not in res):
-                    res.append(temp)
-                else:
-                    mapp.add(nums[j])
+            
+            j = i+1
+            k = len(nums)-1
+
+            while(j < k):
+                summ = nums[i] + nums[j] + nums[k]
+                if(summ == 0):
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while(j<k and nums[j] == nums[j-1]):
+                        j += 1
+                    while(j<k and nums[k] == nums[k+1]):
+                        k -= 1
+                elif(summ > 0):
+                    k -= 1
+                elif(summ < 0):
+                    j += 1
 
         return res
