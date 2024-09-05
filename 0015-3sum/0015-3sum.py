ed032254d56ef1a95802a60caf714c824027276f
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2024-09-05 07:22:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,17 +1,16 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         res = []
         for i in range(0, len(nums)):
-            if(i > 0 and nums[i] == nums[i-1]):
+            if(i != 0 and nums[i] == nums[i-1]):
                 continue
             mapp = set()
             for j in range(i+1, len(nums)):
-                req = 0 - (nums[i]+nums[j])
-                if(req in mapp):
-                    triplet = [nums[i], req, nums[j]]
-                    triplet.sort()
-                    if(triplet not in res):
-                        res.append(triplet)
+                req = -(nums[i] + nums[j])
+                temp = [nums[i], req, nums[j]]
+                if(req in mapp and temp not in res):
+                    res.append(temp)
                 else:
                     mapp.add(nums[j])
 
