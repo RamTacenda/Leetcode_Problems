2ea4bb545b77f2cec641e5b365b537479f4cb060
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2024-10-12 13:20:20 +0530

    Time: 31 ms (90.35%), Space: 16.4 MB (95.32%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,9 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        count = nums.count(val)
-        num = 0
-        while(num < count):
-            nums.remove(val)
-            num += 1
+        temp = []
+        for num in nums:
+            if(num != val):
+                temp.append(num)
+        k = len(temp)
+        for i in range(0, len(temp)):
+            nums[i] = temp[i]
         
-        return len(nums)
+        return k
