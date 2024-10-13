3883941d1c269f910475ff6b82cb2df412a56238
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2024-10-13 12:58:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        mapp = {
+            "I": 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        }
+        res = 0
+        for a, b in zip(s, s[1:]):
+            if(mapp[a] < mapp[b]):
+                res -= mapp[a]
+            else:
+                res += mapp[a]
+        res += mapp[a]
+        return res
