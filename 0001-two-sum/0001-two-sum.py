b03c5e7ed19e9ddf0969244abf6d0fb5c57aec8f
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2024-10-16 20:34:43 +0530

    Time: 27 ms (98.99%), Space: 12.7 MB (14.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,7 +2,7 @@ class Solution(object):
     def twoSum(self, nums, target):
         mapp = dict()
         for i in range(0, len(nums)):
-            req = target - nums[i]
-            if(req in mapp):
-                return [mapp[req], i]
+            rem = target - nums[i]
+            if(rem in mapp):
+                return [mapp[rem], i]
             mapp[nums[i]] = i
