ed5c78aeab802272af7cb83e2d4e232ac98234de
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2024-10-18 20:39:22 +0530

    Time: 1 ms (100%), Space: 16.5 MB (58.52%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,9 +1,24 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        ans = int(dividend/divisor)
-        if(ans > 2**31 -1):
-            return 2**31 -1
-        elif(ans < -2**31):
-            return -2**31
-        else:
-            return ans
+        ans = 0
+        sign = True
+        if(dividend == divisor): return 1
+        if(dividend >= 0 and divisor < 0): sign = False
+        if(dividend < 0 and divisor > 0): sign = False
+
+        n, d = abs(dividend), abs(divisor)
+        while(n >= d):
+            count = 0
+            while(n >= (d << count)):
+                count += 1
+            
+            ans += (1 << (count-1))
+            n -= (d << (count-1))
+
+        if(ans >= 2**31):
+            if(sign):
+                return (2**31)-1
+            else:
+                return -(2**31)
+        
+        return ans if(sign) else -ans
