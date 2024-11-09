8e1697d0a1da08ab2589db39652fd2eaee00bfcd
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2024-11-09 18:19:32 +0530

    Time: 125 ms (80.16%), Space: 58.8 MB (79.6%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,15 +1,15 @@
 class Solution:
-    def combinations(self, idx, ds, n, k, res):
+    def combinations(self, idx, ds, n, k, ans):
         if(len(ds) == k):
-            res.append(ds.copy())
+            ans.append(ds.copy())
             return
 
         for i in range(idx, n+1):
             ds.append(i)
-            self.combinations(i+1, ds, n, k, res)
+            self.combinations(i+1, ds, n, k, ans)
             ds.pop()
 
     def combine(self, n: int, k: int) -> List[List[int]]:
-        res = list()
-        self.combinations(1, list(), n, k, res)
-        return res
+        ans = list()
+        self.combinations(1, list(), n, k, ans)
+        return ans
