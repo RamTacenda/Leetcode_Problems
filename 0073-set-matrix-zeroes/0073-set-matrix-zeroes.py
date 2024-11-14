42d69577e6ca2f0ee8f7e579f4add657f0ac1cfd
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2024-11-14 18:24:08 +0530

    Time: 3 ms (69.78%), Space: 17.4 MB (86.99%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,22 +1,15 @@
 class Solution:
-    def row(self, mat, i, j):
-        for j in range(0, len(mat[0])):
-            if(mat[i][j] != 0):
-                mat[i][j] = "signal"
-
-    def col(self, mat, i, j):
-        for i in range(0, len(mat)):
-            if(mat[i][j] != 0):
-                mat[i][j] = "signal"
-
     def setZeroes(self, mat: List[List[int]]) -> None:
+        row = [False]*(len(mat))
+        col = [False]*(len(mat[0]))
+
         for i in range(0, len(mat)):
             for j in range(0, len(mat[0])):
                 if(mat[i][j] == 0):
-                    self.row(mat, i, j)
-                    self.col(mat, i, j)
+                    row[i] = True
+                    col[j] = True
 
         for i in range(0, len(mat)):
             for j in range(0, len(mat[0])):
-                if(mat[i][j] == "signal"):
+                if(row[i] == True or col[j] == True):
                     mat[i][j] = 0
