8952ad2b26a9d511c76ba2ab76c4ad3b00833ffe
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2024-11-16 08:21:19 +0530

    Time: 23 ms (20.03%), Space: 21.7 MB (21.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
-        check = {chr(i+97): i for i in range(0, 27)}
+        check = {chr(i+97) : i for i in range(0, 27)}
 
         for s in strs:
             count = [0]*(26)
-            
+
             for c in s:
                 count[check[c]] += 1
 
