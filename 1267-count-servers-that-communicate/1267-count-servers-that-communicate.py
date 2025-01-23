0940bf2c1f0791b9cd0edcbbc4c513ccb54b5c0a
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2025-01-23 22:49:13 +0530

    Time: 15 ms (73.78%), Space: 19.5 MB (60.67%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -1,27 +1,18 @@
 class Solution:
-    def check(self, grid, row, col):
-        n, m = len(grid), len(grid[0])
-        # row check
-        for c in range(0, m):
-            if(grid[row][c] == 1 and col != c):
-                return True
-
-        # col check
-        for r in range(0, n):
-            if(grid[r][col] == 1 and row != r):
-                return True
-
-        return False
-
     def countServers(self, grid: List[List[int]]) -> int:
         n, m = len(grid), len(grid[0])
+        row, col = [0]*n, [0]*m
+        for i in range(0, n):
+            for j in range(0, m):
+                if(grid[i][j] == 1):
+                    row[i] += 1
+                    col[j] += 1
+
         count = 0
         for i in range(0, n):
             for j in range(0, m):
                 if(grid[i][j] == 1):
-                    if self.check(grid, i, j):
+                    if(row[i] > 1 or col[j] > 1):
                         count += 1
 
         return count
-
-        
