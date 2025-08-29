eb5d2915cc9478269f1cf8ebb4eaf592fd01e004
RamTacenda <153517227+RamTacenda@users.noreply.github.com>
2025-08-29 12:38:40 +0530

    Time: 19 ms (96.96%), Space: 27 MB (32.26%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,10 +3,10 @@ class Solution:
         minn = float('inf')
         profit = 0
 
-        for i in range(0, len(prices)):
-            if(prices[i] < minn):
-                minn = prices[i]
-            elif(prices[i] - minn > profit):
-                profit = prices[i] - minn
+        for price in prices:
+            if(price < minn):
+                minn = price
+            elif((price-minn) > profit):
+                profit = price-minn
         
         return profit
